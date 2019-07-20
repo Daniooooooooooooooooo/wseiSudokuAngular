080629c9c8b2fbf71f2e3c0e2dc07b4721f88e8e
Daniooooooooooooooooo <daniopila@gmail.com>
2019-07-20 22:04:04 +0200

    Fixed validation

--- src/app/sudoku/sudoku-validation.service.ts
+++ src/app/sudoku/sudoku-validation.service.ts
@@ -11,14 +11,18 @@ export class SudokuValidationService {
   validate(sudokuContent: Sudoku) {
     console.log('Checking...');
     let result = this.checkIfFilled(sudokuContent);
+    console.log('result1: ' + result);
     if (result) {
       result = this.checkRows(sudokuContent);
+      console.log('result:2 ' + result);
     }
     if (result) {
       result = this.checkColumns(sudokuContent);
+      console.log('result3: ' + result);
     }
     if (result) {
       result = this.checkBlocks(sudokuContent);
+      console.log('result4: ' + result);
     }
     console.log('result: ' + result);
     return result;
@@ -28,53 +32,59 @@ export class SudokuValidationService {
     let result = true;
     for (let index = 0; index < 9; index++) {
       let row = sudoku.getRow(index);
-      row.forEach(element => {
-        result = element >= 1 && element <= 9;
+      result = row.every(element => {
+          return element >= 1 && element <= 9;
       });
+      if (result === false) {
+        break;
+      }
     }
     return result;
   }
 
   checkRows(sudoku: Sudoku) {
+    let result = true;
     for (let index = 0; index < 9; index++) {
       let rowSet = new Set();
       let row = sudoku.getRow(index);
-      row.forEach(element => {
+      result = !row.some(element => {
         if (rowSet.has(element)) {
-          return false;
+          return true;
         }
         rowSet.add(element);
       });
-      return true;
+      return result;
     }
   }
 
   checkColumns(sudoku: Sudoku) {
+    let result = true;
     for (let index = 0; index < 9; index++) {
       let columnSet = new Set();
       let column = sudoku.getColumn(index);
-      column.forEach(element => {
+      result = !column.some(element => {
         if (columnSet.has(element)) {
-          return false;
+          return true;
         }
         columnSet.add(element);
       });
-      return true;
+      return result;
     }
   }
 
   checkBlocks(sudoku: Sudoku) {
+    let result = true;
     for (let x = 0; x < 3; x++) {
       for (let y = 0; y < 3; y++) {
       let blockSet = new Set();
       let block = sudoku.getBlock(x, y);
-      block.forEach(element => {
+      result = !block.some(element => {
         if (blockSet.has(element)) {
-          return false;
+          return true;
         }
         blockSet.add(element);
       });
-      return true;
+      return result;
       }
     }
   }
--- src/app/sudoku/sudoku.component.ts
+++ src/app/sudoku/sudoku.component.ts
@@ -14,7 +14,7 @@ export class SudokuComponent implements OnInit {
   ngOnInit() {
     this.sudoku = new Sudoku();
     console.log(this.sudoku);
-    this.sudoku.initValues('530070000600195000098000060800060003400803001700020006060000280000419005000080079');
+    this.sudoku.initValues('534678912672195348198342567859761423426853791713924856961537284287419635345286179');
   }
 
   check() {
